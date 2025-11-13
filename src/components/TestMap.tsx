25c21ac97ad5d3c414d8a47809902ddde0cf07f8
marcuscastelo <marcuscastelo@usp.br>
2025-11-12 22:45:43 -0300

    feat(map-ui): implement user geolocation and update map center accordingly

--- src/components/TestMap.tsx
+++ src/components/TestMap.tsx
@@ -1,6 +1,13 @@
 import { Marker } from '@googlemaps/markerclusterer'
 import { AdvancedMarker, APIProvider, InfoWindow, Map } from 'solid-google-maps'
-import { createEffect, createMemo, createSignal, For, Show } from 'solid-js'
+import {
+  createEffect,
+  createMemo,
+  createSignal,
+  For,
+  onMount,
+  Show,
+} from 'solid-js'
 
 import { ClusteredMarkers } from '~/components/ClusteredMarkers'
 import { API_KEY } from '~/utils/env'
@@ -42,6 +49,42 @@ export function TestMap() {
   const [trees, setTrees] = createSignal<Tree[]>()
   const [selectedCategory] = createSignal<string | null>(null)
 
+  const [lastUserLocation] = createSignal<google.maps.LatLngLiteral | null>({
+    lat: 41.544581,
+    lng: -8.427375,
+  })
+
+  const [userLocation, setUserLocation] =
+    // eslint-disable-next-line solid/reactivity
+    createSignal<google.maps.LatLngLiteral | null>(lastUserLocation())
+
+  let mapRef: google.maps.Map | null = null
+
+  onMount(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          })
+          if (mapRef) {
+            alert('Recent location found!')
+            mapRef.setCenter({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            })
+          }
+        },
+        (error) => {
+          console.error('Error getting user location:', error)
+        },
+      )
+    } else {
+      console.error('Geolocation is not supported by this browser.')
+    }
+  })
+
   // load data asynchronously
   createEffect(() => {
     loadTreeDataset()
@@ -71,12 +114,13 @@ export function TestMap() {
   return (
     <APIProvider apiKey={API_KEY}>
       <Map
-        style={{ height: '500px', width: '100%' }}
-        mapId={'bf51a910020fa25a'}
-        defaultCenter={{ lat: 43.64, lng: -79.41 }}
-        defaultZoom={10}
+        style={{ height: '100vh', width: '100vw' }}
+        mapId={'40527d464f34e7febe80350b'}
+        defaultCenter={userLocation() || { lat: 22.54992, lng: 0 }}
+        defaultZoom={14}
         gestureHandling={'greedy'}
         disableDefaultUI
+        ref={(map) => (mapRef = map)}
       >
         <ClusteredMarkers ref={setMarkers}>
           {(addToCluster) => (
