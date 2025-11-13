52c28384d4569a2d7238877fbfaf9b65cb66800c
marcuscastelo <marcuscastelo@usp.br>
2025-11-12 22:55:22 -0300

    feat(config): add SSL certificate and private key for HTTPS server

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
-
+import fs from 'fs';
 export default defineConfig({
   ssr: false,
   vite: {
-    plugins: [tailwindcss()],
+    server: {
+      https: {
+        key: fs.readFileSync('./.cert/key.pem'),
+        cert: fs.readFileSync('./.cert/cert.pem'),
+      }
+    },
+    plugins: [
+      tailwindcss(),
+    ],
     ssr: {
       noExternal: ['@googlemaps/markerclusterer']
-    }
+    },
   }
 });
