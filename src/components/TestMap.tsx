23a48b52d6308bc60704dc16005bef10863b95fe
marcuscastelo <marcuscastelo@usp.br>
2025-11-13 16:12:10 -0300

    refactor(TestMap-ui): remove unused imports and commented-out code

--- src/components/TestMap.tsx
+++ src/components/TestMap.tsx
@@ -1,18 +1,8 @@
-import { Marker } from '@googlemaps/markerclusterer'
 import { Key } from '@solid-primitives/keyed'
-import { scheduleIdle } from '@solid-primitives/scheduled'
-import {
-  AdvancedMarker,
-  AdvancedMarkerAnchorPoint,
-  APIProvider,
-  InfoWindow,
-  Map,
-  MapCameraChangedEvent,
-} from 'solid-google-maps'
-import { createEffect, createMemo, createSignal, For, Show } from 'solid-js'
+import { AdvancedMarker, APIProvider, Map } from 'solid-google-maps'
+import { createEffect, createSignal, Show } from 'solid-js'
 import { ClusterProperties } from 'supercluster'
 
-import { ClusteredMarkers } from '~/components/ClusteredMarkers'
 import { POIBasic } from '~/hooks/usePOI'
 import { useSupercluster } from '~/hooks/useSupercluster'
 import * as POI from '~/poi.json'
@@ -126,16 +116,6 @@ export function TestMap() {
       .catch(console.error)
   })
 
-  const [selectedTreeKey, setSelectedTreeKey] = createSignal<string | null>(
-    null,
-  )
-  const selectedTree = createMemo(() =>
-    selectedTreeKey()
-      ? (trees()?.find((t) => t.id === selectedTreeKey()) ?? null)
-      : null,
-  )
-  const [markers, setMarkers] = createSignal<{ [key: string]: Marker }>({})
-
   return (
     <APIProvider apiKey={API_KEY}>
       <Map
@@ -157,7 +137,9 @@ export function TestMap() {
               feature().properties as ClusterProperties
             const isCluster = () => clusterProperties().cluster
             const markerSize = () =>
-              Math.floor(48 + Math.sqrt(clusterProperties().point_count) * 2)
+              Math.floor(
+                48 * 2 + Math.sqrt(clusterProperties().point_count) * 2,
+              )
 
             if (isNaN(position().lat) || isNaN(position().lng)) {
               return null
@@ -169,15 +151,63 @@ export function TestMap() {
                   <AdvancedMarker
                     position={position()}
                     zIndex={clusterProperties().point_count}
-                    anchorPoint={AdvancedMarkerAnchorPoint.CENTER}
-                    style={{
-                      width: `${markerSize()}px`,
-                      height: `${markerSize()}px`,
-                    }}
-                    class="marker cluster"
+                    // anchorPoint={AdvancedMarkerAnchorPoint.CENTER}
+                    // style={{
+                    //   width: `${markerSize()}px`,
+                    //   height: `${markerSize()}px`,
+                    // }}
+                    // class="marker cluster"
                   >
-                    {isCluster() ? '[C]' : '[F]'} ({position().lat.toFixed(2)},{' '}
-                    {position().lng.toFixed(2)})
+                    {/* {isCluster() ? '[C]' : '[F]'} ({position().lat.toFixed(2)},{' '}
+                    {position().lng.toFixed(2)}) */}
+                    <svg
+                      id="svg3512"
+                      viewBox="0 0 80 80"
+                      version="1.0"
+                      y="0"
+                      x="0"
+                    >
+                      <g id="layer1">
+                        <g
+                          id="g3535"
+                          transform="matrix(.96875 0 0 .98751 1.2501 -.25079)"
+                        >
+                          <path
+                            id="path2742"
+                            style="fill-rule:evenodd;fill:#000000"
+                            d="m54.962 37.281l17.138-10.262 7.678 14.835c1.855 8.105-8.309 11.079-16.47 10.596l-8.346-15.169z"
+                          />
+                          <path
+                            id="path2743"
+                            style="fill-rule:evenodd;fill:#000000"
+                            d="m51.067 47.877l-9.013 15.95 9.013 16.173 0.223-6.469h8.235c3.005 0.26 6.899-1.711 8.346-4.573l10.572-19.408c-3.487 3.458-7.971 4.35-13.131 4.35h-13.911l-0.334-6.023z"
+                          />
+                          <path
+                            id="path2751"
+                            style="fill-rule:evenodd;fill:#000000"
+                            d="m30.928 28.212l-17.269-10.037 9.213-13.93c6.17-5.5589 13.698 1.9041 17.237 9.29l-9.181 14.677z"
+                          />
+                          <path
+                            id="path2752"
+                            style="fill-rule:evenodd;fill:#000000"
+                            d="m42.062 26.482l18.288 0.157 9.732-15.749-5.751 2.945-3.997-7.2167c-1.231-2.7593-4.84-5.216-8.04-5.0942l-22.06 0.156c4.708 1.3773 7.663 4.8732 10.168 9.3959l6.752 12.19-5.092 3.216z"
+                          />
+                          <path
+                            id="path2753"
+                            style="fill-rule:evenodd;fill:#000000"
+                            d="m0.44401 27.381l5.3508 4.101-5.036 9.464c-2.5181 4.365 1.8077 8.616 4.5639 10.096 2.7133 1.457 6.9243 1.63 10.859 1.577l7.082-11.357 5.35 2.839-9.285-16.878-18.885 0.158z"
+                          />
+                          <path
+                            id="path2754"
+                            style="fill-rule:evenodd;fill:#000000"
+                            d="m1.2309 49.307l11.488 20.821c2.309 2.892 6.663 3.576 11.174 3.471h12.118v-19.718l-22.977-0.158c-3.5671 0.211-8.2359-0.525-11.803-4.416z"
+                          />
+                        </g>
+                      </g>
+                    </svg>
+                    <span class="text-xl w-full bg-black/20 text-center">
+                      {clusterProperties().point_count_abbreviated}
+                    </span>
                   </AdvancedMarker>
                 </Show>
                 {/* <Show when={!isCluster()}>
@@ -193,14 +223,14 @@ export function TestMap() {
             )
           }}
         </Key>
-        <Show when={selectedTreeKey() && markers()[selectedTreeKey()!]}>
+        {/* <Show when={selectedTreeKey() && markers()[selectedTreeKey()!]}>
           <InfoWindow
             anchor={markers()[selectedTreeKey()!]}
             onCloseClick={() => setSelectedTreeKey(null)}
           >
             {selectedTree()?.slug}
           </InfoWindow>
-        </Show>
+        </Show> */}
       </Map>
     </APIProvider>
   )
