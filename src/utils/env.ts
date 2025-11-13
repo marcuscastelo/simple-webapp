77625fd916812137f25e74f5e93bb67186f5744f
marcuscastelo <marcuscastelo@usp.br>
2025-11-13 20:46:10 -0300

    fix(env): validate environment variables on app mount

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,11 +5,14 @@ const envVars = [
   'VITE_PUBLIC_SUPABASE_URL',
 ] as const
 
-envVars.forEach((varName) => {
-  if (!import.meta.env[varName]) {
-    throw new Error(`${varName} is not defined in environment variables`)
-  }
-})
+export function validateEnvVars() {
+  envVars.forEach((varName) => {
+    if (!import.meta.env[varName]) {
+      console.error(`${varName} is not defined in environment variables`)
+      throw new Error(`${varName} is not defined in environment variables`)
+    }
+  })
+}
 
 export const env: Record<(typeof envVars)[number], string> = {
   VITE_GOOGLE_MAPS_API_KEY: import.meta.env.VITE_GOOGLE_MAPS_API_KEY!,
