d6ec9376b1eb7086c2dec373b1f1cb806e10832a
marcuscastelo <marcuscastelo@usp.br>
2025-11-24 11:37:27 -0300

    rename(hooks): remove backwards-compatibility shim for useCoordinatesFromUrl

--- src/modules/collection-points/hooks/useCoordinatesFromUrl.ts
+++
@@ -1,3 +0,0 @@
-// Backwards-compatibility shim for the renamed hook
-export type { MapUrlParams } from './useMapUrlParams'
-export { useMapUrlParams as useCoordinatesFromUrl } from './useMapUrlParams'
